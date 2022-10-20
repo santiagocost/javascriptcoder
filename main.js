74df6eccee2ef67a12a56d3120b9c3039b447699
santiagocost <scostan9@gmail.com>
2022-10-20 11:57:39 -0300

    segunda entrega

--- main.js
+++ main.js
@@ -1,17 +1,45 @@
-function calculandoPromedio (num1, num2, num3) {
-    let promedio = (num1 + num2 + num3)/4;
-    return 'El promedio es: ' + promedio + ' pesos';
-}
-console.log(calculandoPromedio(50, 200, 30));
+var nombre = prompt ('¿Cuál es tu nombre?')
 
-for (let i= 1; i <=25; i++) {
-    console.log('iteraciones');
-}
-console.log ('Mostrar ciclo');
+class Juego {
+  constructor(nombre, precio) {
+    this.nombre = nombre;
+    this.precio = precio;
+    this.cantidad = 20;
+  }
+  vender() {
+    if (this.cantidad > 0) {
+      this.cantidad--;
+    } else {
+      console.log("No hay más stock!");
+    }
+  }
 
-let pelicula = prompt ("Elije tu película favorita");
-if (pelicula == 'terror') {
-    alert ("No hay películas de ese género");
-} else {
-    alert ("Aquí tienes")
+  agregarIva() {
+    this.precio = this.precio * 1.22;
+  }
+
+  agregarStock(cantidad){
+    this.cantidad = this.cantidad + cantidad;
+  }
 }
+
+const gowRagnarok = new Juego("God of War Ragnarok", 4000);
+const pkmnSyv = new Juego("Pokémon Scarlet & Violet", 4500);
+
+const stockJuegos = [gowRagnarok, pkmnSyv];
+console.log(stockJuegos);
+
+for(let juegos of stockJuegos) {
+    console.log (juegos);
+}
+
+function aumentarStockGow(arrayJuegos) {
+    for(let juego of arrayJuegos) {
+        if(juego.nombre === "God of War Ragnarok") {
+            juego.agregarStock(50);
+        }
+    } 
+}
+
+aumentarStockGow(stockJuegos);
+console.log(stockJuegos);
