f9d6cb90fa4150c47596c8595116fc084f347899
santiagocost <scostan9@gmail.com>
2022-11-08 11:09:44 -0300

    agregando json y eventos

--- main.js
+++ main.js
@@ -1,45 +1,303 @@
-var nombre = prompt ('¿Cuál es tu nombre?')
+const stockProductos = [
+  {
+    id: 1,
+    nombre: "God of War Ragnarok",
+    cantidad: 1,
+    desc: "Secuela nórdica del fantasma de Esparta",
+    precio: 80,
+    img: "img/gow.jpg",
+  },
+  {
+    id: 2,
+    nombre: "Pokémon Escarlata",
+    cantidad: 1,
+    desc: "Nueva aventura del extenso mundo Pokémon",
+    precio: 90,
+    img: "img/pkmnes.png",
+  },
+  {
+    id: 3,
+    nombre: "Pokémon Púrpura",
+    cantidad: 1,
+    desc: "Nueva aventura del extenso mundo Pokémon",
+    precio: 90,
+    img: "img/pkmnv.png",
+  },
+  {
+    id: 4,
+    nombre: "Harry Potter Hogwarts Legacy",
+    cantidad: 1,
+    desc: "Vive la experiencia de ser un mago y adentráte a lo más profundo de Hogwarts",
+    precio: 80,
+    img: "img/harrypo.webp",
+  },
+  {
+    id: 5,
+    nombre: "The Callisto Protocol",
+    cantidad: 1,
+    desc: "¿Dead Space? Mejorado",
+    precio: 80,
+    img: "img/callisto.jpg",
+  },
+  {
+    id: 6,
+    nombre: "Fifa 23",
+    cantidad: 1,
+    desc: "El mismo que el 22 pero con otro número",
+    precio: 60,
+    img: "img/fifa23.jpg",
+  },
+  {
+    id: 7,
+    nombre: "Horizon Forbidden West",
+    cantidad: 1,
+    desc: "Mejores gráficos, más robots",
+    precio: 70,
+    img: "img/horizon2.jpg",
+  },
+  {
+    id: 8,
+    nombre: "Resident Evil 4 Remake",
+    cantidad: 1,
+    desc: "Difícil superar el original",
+    precio: 80,
+    img: "img/resident4.jpg",
+  },
+  {
+    id: 9,
+    nombre: "Splatoon 3",
+    cantidad: 1,
+    desc: "Pintamos toda la casa",
+    precio: 70,
+    img: "img/splatoon3.jpg",
+  },
+  {
+    id: 10,
+    nombre: "Xenoblade Chronicles 3",
+    cantidad: 1,
+    desc: "Juego de rol y aventuras",
+    precio: 70,
+    img: "img/xenoblade3.jpg",
+  },
+];
 
-class Juego {
-  constructor(nombre, precio) {
-    this.nombre = nombre;
-    this.precio = precio;
-    this.cantidad = 20;
-  }
-  vender() {
-    if (this.cantidad > 0) {
-      this.cantidad--;
+let carrito = [];
+
+const contenedor = document.querySelector("#contenedor");
+const carritoContenedor = document.querySelector("#carritoContenedor");
+const vaciarCarrito = document.querySelector("#vaciarCarrito");
+const precioTotal = document.querySelector("#precioTotal");
+const activarFuncion = document.querySelector("#activarFuncion");
+const procesarCompra = document.querySelector("#procesarCompra");
+const totalProceso = document.querySelector("#totalProceso");
+const formulario = document.querySelector("#procesar-pago");
+
+if (activarFuncion) {
+  activarFuncion.addEventListener("click", procesarPedido);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+  mostrarCarrito();
+  document.querySelector("#activarFuncion").click(procesarPedido);
+});
+if (formulario) {
+  formulario.addEventListener("submit", enviarCompra);
+}
+
+if (vaciarCarrito) {
+  vaciarCarrito.addEventListener("click", () => {
+    carrito.length = [];
+    mostrarCarrito();
+  });
+}
+
+if (procesarCompra) {
+  procesarCompra.addEventListener("click", () => {
+    if (carrito.length === 0) {
+      Swal.fire({
+        title: "¡Tu carrito está vacio!",
+        text: "Compra algo para continuar con la compra",
+        icon: "error",
+        confirmButtonText: "Aceptar",
+      });
     } else {
-      console.log("No hay más stock!");
+      location.href = "compra.html";
     }
-  }
-
-  agregarIva() {
-    this.precio = this.precio * 1.22;
-  }
-
-  agregarStock(cantidad){
-    this.cantidad = this.cantidad + cantidad;
-  }
+  });
 }
 
-const gowRagnarok = new Juego("God of War Ragnarok", 4000);
-const pkmnSyv = new Juego("Pokémon Scarlet & Violet", 4500);
+stockProductos.forEach((prod) => {
+  const { id, nombre, precio, desc, img, cantidad } = prod;
+  if (contenedor) {
+    contenedor.innerHTML += `
+    <div class="card mt-3" style="width: 18rem;">
+    <img class="card-img-top mt-2" src="${img}" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">${nombre}</h5>
+      <p class="card-text">Precio: ${precio}</p>
+      <p class="card-text">Descripción: ${desc}</p>
+      <p class="card-text">Cantidad: ${cantidad}</p>
+      <button class="btn btn-primary" onclick="agregarProducto(${id})">Comprar Producto</button>
+    </div>
+  </div>
+    `;
+  }
+});
 
-const stockJuegos = [gowRagnarok, pkmnSyv];
-console.log(stockJuegos);
+const agregarProducto = (id) => {
+  const existe = carrito.some((prod) => prod.id === id);
 
-for(let juegos of stockJuegos) {
-    console.log (juegos);
+  if (existe) {
+    const prod = carrito.map((prod) => {
+      if (prod.id === id) {
+        prod.cantidad++;
+      }
+    });
+  } else {
+    const item = stockProductos.find((prod) => prod.id === id);
+    carrito.push(item);
+  }
+  mostrarCarrito();
+};
+
+const mostrarCarrito = () => {
+  const modalBody = document.querySelector(".modal .modal-body");
+  if (modalBody) {
+    modalBody.innerHTML = "";
+    carrito.forEach((prod) => {
+      const { id, nombre, precio, desc, img, cantidad } = prod;
+      console.log(modalBody);
+      modalBody.innerHTML += `
+      <div class="modal-contenedor">
+        <div>
+        <img class="img-fluid img-carrito" src="${img}"/>
+        </div>
+        <div>
+        <p>Producto: ${nombre}</p>
+      <p>Precio: ${precio}</p>
+      <p>Cantidad :${cantidad}</p>
+      <button class="btn btn-danger"  onclick="eliminarProducto(${id})">Eliminar producto</button>
+        </div>
+      </div>
+       `;
+    });
+  }
+  if (carrito.length === 0) {
+    console.log("Nada");
+    modalBody.innerHTML = `
+    <p class="text-center text-primary parrafo">¡Aun no agregaste nada!</p>
+    `;
+  } else {
+    console.log("Algo");
+  }
+  carritoContenedor.textContent = carrito.length;
+
+  if (precioTotal) {
+    precioTotal.innerText = carrito.reduce(
+      (acc, prod) => acc + prod.cantidad * prod.precio,
+      0
+    );
+  }
+
+  guardarStorage();
+};
+
+function guardarStorage() {
+  localStorage.setItem("carrito", JSON.stringify(carrito));
 }
 
-function aumentarStockGow(arrayJuegos) {
-    for(let juego of arrayJuegos) {
-        if(juego.nombre === "God of War Ragnarok") {
-            juego.agregarStock(50);
-        }
-    } 
+function eliminarProducto(id) {
+  const juegoId = id;
+  carrito = carrito.filter((juego) => juego.id !== juegoId);
+  mostrarCarrito();
+}
+function procesarPedido() {
+  carrito.forEach((prod) => {
+    const listaCompra = document.querySelector("#lista-compra tbody");
+    const { id, nombre, precio, img, cantidad } = prod;
+    if (listaCompra) {
+      const row = document.createElement("tr");
+      row.innerHTML += `
+              <td>
+              <img class="img-fluid img-carrito" src="${img}"/>
+              </td>
+              <td>${nombre}</td>
+            <td>${precio}</td>
+            <td>${cantidad}</td>
+            <td>${precio * cantidad}</td>
+            `;
+      listaCompra.appendChild(row);
+    }
+  });
+  totalProceso.innerText = carrito.reduce(
+    (acc, prod) => acc + prod.cantidad * prod.precio,
+    0
+  );
 }
 
-aumentarStockGow(stockJuegos);
-console.log(stockJuegos);
+function enviarCompra(e) {
+  e.preventDefault();
+  const cliente = document.querySelector("#cliente").value;
+  const email = document.querySelector("#correo").value;
+
+  if (email === "" || cliente == "") {
+    Swal.fire({
+      title: "¡Debes completar tu email y nombre!",
+      text: "Rellena el formulario",
+      icon: "error",
+      confirmButtonText: "Aceptar",
+    });
+  } else {
+    const btn = document.getElementById("button");
+
+    // document.getElementById('procesar-pago')
+    //  .addEventListener('submit', function(event) {
+    //    event.preventDefault();
+
+    btn.value = "Enviando...";
+
+    const serviceID = "default_service";
+    const templateID = "template_qxwi0jn";
+
+    emailjs.sendForm(serviceID, templateID, this).then(
+      () => {
+        btn.value = "Finalizar compra";
+        alert("Correo enviado!");
+      },
+      (err) => {
+        btn.value = "Finalizar compra";
+        alert(JSON.stringify(err));
+      }
+    );
+
+    const spinner = document.querySelector("#spinner");
+    spinner.classList.add("d-flex");
+    spinner.classList.remove("d-none");
+
+    setTimeout(() => {
+      spinner.classList.remove("d-flex");
+      spinner.classList.add("d-none");
+      formulario.reset();
+
+      const alertExito = document.createElement("p");
+      alertExito.classList.add(
+        "alert",
+        "alerta",
+        "d-block",
+        "text-center",
+        "col-12",
+        "mt-2",
+        "alert-success"
+      );
+      alertExito.textContent = "Compra realizada correctamente";
+      formulario.appendChild(alertExito);
+
+      setTimeout(() => {
+        alertExito.remove();
+      }, 3000);
+    }, 3000);
+  }
+  localStorage.clear();
+}
