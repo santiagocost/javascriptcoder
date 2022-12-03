d1a442390dac87e66c758342ff2a168ad64bb424
santiagocost <scostan9@gmail.com>
2022-12-03 20:16:21 -0300

    ultimos cambios

--- main.js
+++ main.js
@@ -1,86 +1,29 @@
-const stockProductos = [
-  {
-    id: 1,
-    nombre: "God of War Ragnarok",
-    cantidad: 1,
-    desc: "Secuela nórdica del fantasma de Esparta",
-    precio: 80,
-    img: "img/gow.jpg",
-  },
-  {
-    id: 2,
-    nombre: "Pokémon Escarlata",
-    cantidad: 1,
-    desc: "Nueva aventura del extenso mundo Pokémon",
-    precio: 90,
-    img: "img/pkmnes.png",
-  },
-  {
-    id: 3,
-    nombre: "Pokémon Púrpura",
-    cantidad: 1,
-    desc: "Nueva aventura del extenso mundo Pokémon",
-    precio: 90,
-    img: "img/pkmnv.png",
-  },
-  {
-    id: 4,
-    nombre: "Harry Potter Hogwarts Legacy",
-    cantidad: 1,
-    desc: "Vive la experiencia de ser un mago y adentráte a lo más profundo de Hogwarts",
-    precio: 80,
-    img: "img/harrypo.webp",
-  },
-  {
-    id: 5,
-    nombre: "The Callisto Protocol",
-    cantidad: 1,
-    desc: "¿Dead Space? Mejorado",
-    precio: 80,
-    img: "img/callisto.jpg",
-  },
-  {
-    id: 6,
-    nombre: "Fifa 23",
-    cantidad: 1,
-    desc: "El mismo que el 22 pero con otro número",
-    precio: 60,
-    img: "img/fifa23.jpg",
-  },
-  {
-    id: 7,
-    nombre: "Horizon Forbidden West",
-    cantidad: 1,
-    desc: "Mejores gráficos, más robots",
-    precio: 70,
-    img: "img/horizon2.jpg",
-  },
-  {
-    id: 8,
-    nombre: "Resident Evil 4 Remake",
-    cantidad: 1,
-    desc: "Difícil superar el original",
-    precio: 80,
-    img: "img/resident4.jpg",
-  },
-  {
-    id: 9,
-    nombre: "Splatoon 3",
-    cantidad: 1,
-    desc: "Pintamos toda la casa",
-    precio: 70,
-    img: "img/splatoon3.jpg",
-  },
-  {
-    id: 10,
-    nombre: "Xenoblade Chronicles 3",
-    cantidad: 1,
-    desc: "Juego de rol y aventuras",
-    precio: 70,
-    img: "img/xenoblade3.jpg",
-  },
-];
+const stockProductosJson = "json/productos.json";
+var productos = [];
+fetch (stockProductosJson)
+  .then (Response => Response.json())
+  .then (stockProductos => {
+    productos = stockProductos;
+    stockProductos.forEach((prod) => {
+      const { id, nombre, precio, desc, img, cantidad } = prod;
+      if (contenedor) {
+        contenedor.innerHTML += `
+        <div class="card mt-3" style="width: 18rem;">
+        <img class="card-img-top mt-2" src="${img}" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">${nombre}</h5>
+          <p class="card-text">Precio: ${precio}</p>
+          <p class="card-text">Descripción: ${desc}</p>
+          <p class="card-text">Cantidad: ${cantidad}</p>
+          <button class="btn btn-primary" onclick="agregarProducto(${id})">Comprar Producto</button>
+        </div>
+      </div>
+        `;
+      }
+    });
+  });
 
+  
 let carrito = [];
 
 const contenedor = document.querySelector("#contenedor");
@@ -128,23 +71,6 @@ if (procesarCompra) {
   });
 }
 
-stockProductos.forEach((prod) => {
-  const { id, nombre, precio, desc, img, cantidad } = prod;
-  if (contenedor) {
-    contenedor.innerHTML += `
-    <div class="card mt-3" style="width: 18rem;">
-    <img class="card-img-top mt-2" src="${img}" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">${nombre}</h5>
-      <p class="card-text">Precio: ${precio}</p>
-      <p class="card-text">Descripción: ${desc}</p>
-      <p class="card-text">Cantidad: ${cantidad}</p>
-      <button class="btn btn-primary" onclick="agregarProducto(${id})">Comprar Producto</button>
-    </div>
-  </div>
-    `;
-  }
-});
 
 const agregarProducto = (id) => {
   const existe = carrito.some((prod) => prod.id === id);
@@ -156,7 +82,7 @@ const agregarProducto = (id) => {
       }
     });
   } else {
-    const item = stockProductos.find((prod) => prod.id === id);
+    const item = productos.find((prod) => prod.id === id);
     carrito.push(item);
   }
   mostrarCarrito();
@@ -237,67 +163,3 @@ function procesarPedido() {
   );
 }
 
-function enviarCompra(e) {
-  e.preventDefault();
-  const cliente = document.querySelector("#cliente").value;
-  const email = document.querySelector("#correo").value;
-
-  if (email === "" || cliente == "") {
-    Swal.fire({
-      title: "¡Debes completar tu email y nombre!",
-      text: "Rellena el formulario",
-      icon: "error",
-      confirmButtonText: "Aceptar",
-    });
-  } else {
-    const btn = document.getElementById("button");
-
-    // document.getElementById('procesar-pago')
-    //  .addEventListener('submit', function(event) {
-    //    event.preventDefault();
-
-    btn.value = "Enviando...";
-
-    const serviceID = "default_service";
-    const templateID = "template_qxwi0jn";
-
-    emailjs.sendForm(serviceID, templateID, this).then(
-      () => {
-        btn.value = "Finalizar compra";
-        alert("Correo enviado!");
-      },
-      (err) => {
-        btn.value = "Finalizar compra";
-        alert(JSON.stringify(err));
-      }
-    );
-
-    const spinner = document.querySelector("#spinner");
-    spinner.classList.add("d-flex");
-    spinner.classList.remove("d-none");
-
-    setTimeout(() => {
-      spinner.classList.remove("d-flex");
-      spinner.classList.add("d-none");
-      formulario.reset();
-
-      const alertExito = document.createElement("p");
-      alertExito.classList.add(
-        "alert",
-        "alerta",
-        "d-block",
-        "text-center",
-        "col-12",
-        "mt-2",
-        "alert-success"
-      );
-      alertExito.textContent = "Compra realizada correctamente";
-      formulario.appendChild(alertExito);
-
-      setTimeout(() => {
-        alertExito.remove();
-      }, 3000);
-    }, 3000);
-  }
-  localStorage.clear();
-}
